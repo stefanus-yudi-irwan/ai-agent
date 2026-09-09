c2b00d05e3afbaa5477cb5664b10b02946ecaee7
Stefanus Yudi Irwan <stefanus.yudi96@gmail.com>
2026-09-09 23:08:28 +0700

    refactor: refactor unused tools and llm-client

--- component/tools/email_sender/smtp/smtp_config.py
+++
@@ -1,9 +0,0 @@
-"""config for smtp email sender"""
-from dataclasses import dataclass
-
-@dataclass
-class SMTPSenderConfig:
-    """class for smtp email sender config"""
-    smtp_server: str
-    app_password: str
-    sender_email: str
--- component/tools/notification_pusher/pushover_config.py
+++
@@ -1,9 +0,0 @@
-"""script for pushover configuration"""
-from dataclasses import dataclass
-
-@dataclass
-class PushOverConfig:
-    """class for pushover notification"""
-    token: str
-    user: str
-    url: str
